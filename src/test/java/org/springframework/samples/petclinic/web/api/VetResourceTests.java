8d135643fa4b00e04456fb5023ea76ea87daf6fa
John Freeman <john.freeman.opensource@gmail.com>
2019-08-16 14:33:18 +0100

    Upgrade to Spring Boot 2.1.7
    Keeping up with the latest changes.

--- src/test/java/org/springframework/samples/petclinic/web/api/VetResourceTests.java
+++ src/test/java/org/springframework/samples/petclinic/web/api/VetResourceTests.java
@@ -36,7 +36,7 @@ public class VetResourceTests {
 
 		given(clinicService.findVets()).willReturn(Arrays.asList(vet));
 
-		mvc.perform(get("/api/vets.json") //
+		mvc.perform(get("/api/vets") //
 				.accept(MediaType.APPLICATION_JSON)) //
 				.andExpect(status().isOk()) //
 				.andExpect(jsonPath("$[0].id").value(1));
