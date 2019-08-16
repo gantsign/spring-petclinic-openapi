ebf52865e9db55bda3cd47e3f327d9d88367b40a
John Freeman <john.freeman.opensource@gmail.com>
2019-08-16 19:50:20 +0100

    Remove redundant code
    Leftover from Spring MVC implementation.

--- src/main/java/org/springframework/samples/petclinic/web/api/FailingApiController.java
+++ src/main/java/org/springframework/samples/petclinic/web/api/FailingApiController.java
@@ -1,6 +1,5 @@
 package org.springframework.samples.petclinic.web.api;
 
-import org.springframework.samples.petclinic.web.CrashController;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -10,11 +9,10 @@ import org.springframework.web.bind.annotation.RestController;
 * Controller used to showcase what happens when an exception is thrown
 *
 * @author NilsHartmann
-* @see CrashController
 */
 @RestController
 public class FailingApiController extends AbstractResourceController {
-	
+
 	@GetMapping("/oups")
 	@ResponseBody
 	String failingRequest() {
