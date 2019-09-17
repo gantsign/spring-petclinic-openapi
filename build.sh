efee252bd7312a844ff48451e32916dd05e7e9e7
John Freeman <john.freeman.opensource@gmail.com>
2019-09-17 12:31:10 +0100

    Fix Travis CI build
    Now using Gradle.

---
+++ build.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -e
+
+./gradlew clean build --no-daemon
+
+(cd client/generated && npm install && npm run build)
+
+(cd client && npm install && npm run build && CI=true npm run test)
+
+if [[ "${CI:-}" == 'true' ]] && [[ $(git status --porcelain=v1 --untracked-files=no |
+        grep --invert-match 'package-lock.json') ]]; then
+    echo 'Source files not properly formatted, reformat and push again.' 1>&2
+    git status --porcelain=v1 --untracked-files=no |
+        grep --invert-match 'package-lock.json'
+    exit 1
+fi
